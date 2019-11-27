5086c61e2c8f5daee748bf6319b3dde39cab5884
Jaffri <fjaffri@deloitte.com>
2019-11-27 21:57:34 +0530

    project setup with starter dependencies

---
+++ src/main/java/com/sgeneral/employee/SocieteGeneralDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sgeneral.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocieteGeneralDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocieteGeneralDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgeneral/employee/SocieteGeneralDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgeneral.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocieteGeneralDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
